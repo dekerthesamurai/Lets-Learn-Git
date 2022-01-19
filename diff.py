459e57d8d4f81cea593d2001c31c8f448b88b8d3
dekerthesamurai <deker27072002@gmail.com>
2022-01-19 13:46:43 +0530

    adding diff.py

---
+++ diff.py
@@ -0,0 +1,8 @@
+def diff(a,b):
+    tot = a - b
+    return tot
+
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+print("The difference of entered two numbers is :",diff(a,b))
