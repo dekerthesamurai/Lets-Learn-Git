8e629004b21858333c4a97b3e285e8be59166f01
dekerthesamurai <deker27072002@gmail.com>
2022-01-19 15:17:54 +0530

    added multiply.py

---
+++ multiply.py
@@ -0,0 +1,8 @@
+def mult(a,b):
+    tot = a * b
+    return tot 
+
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+print("The product of entered two numbers is :",mult(a,b))
